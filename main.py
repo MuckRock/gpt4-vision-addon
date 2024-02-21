cc7efb7ff934a72b0f538521b64fd83eaa89f8e8
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-02-21 16:01:55 -0600

    Add validate() logic

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import zipfile
 from typing import Annotated, Any, List
 from io import StringIO
 from documentcloud.addon import AddOn
+from documentcloud.exceptions import APIError
 from openai import OpenAI
 from pydantic import (
     BaseModel,
@@ -25,8 +26,51 @@ import pandas as pd
 class Vision(AddOn):
     """Extract tabular data with GPT4-Vision"""
 
+    def calculate_cost(self, documents):
+        """ Given a set of documents, counts the number of pages and returns a cost"""
+        total_num_pages = 0
+        for doc in documents:
+            start_page = self.data.get("start_page", 1)
+            end_page = self.data.get("end_page")
+            last_page = 0
+            if end_page <= doc.page_count:
+                last_page = end_page
+            else:
+                last_page = doc.page_count
+            pages_to_analyze = last_page - start_page
+            total_num_pages += pages_to_analyze
+        cost = total_num_pages
+        return cost
+
+
+    def validate(self):
+        """Validate that we can run the analysis"""
+
+        if self.get_document_count() is None:
+            self.set_message(
+                "It looks like no documents were selected. Search for some or "
+                "select them and run again."
+            )
+            sys.exit(0)
+        if not self.org_id:
+            self.set_message("No organization to charge.")
+            sys.exit(0)
+        ai_credit_cost = self.calculate_cost(
+            self.get_documents()
+        )
+        try:
+            self.charge_credits(ai_credit_cost)
+        except ValueError:
+            return False
+        except APIError:
+            return False
+        return True
+
     def main(self):
         """The main add-on functionality goes here."""
+        if not self.validate():
+            self.set_message("You do not have sufficient AI credits to run this Add-On on this document set")
+            sys.exit(0)
         default_prompt_text = """
             Take a moment to reason about the best set of headers for the tables.
             Write a good h1 for the image above. Then follow up with a short description of the what the data is about.
